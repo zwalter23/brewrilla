996cb19ea4e207ca8846d40bae18ffe970193796
catastroface <barna.urmossy@gmail.com>
2021-08-30 18:47:01 +0200

    Implement pagination logic

--- src/components/BeerCollection.js
+++ src/components/BeerCollection.js
@@ -1,6 +1,14 @@
 import React from "react";
 
-export default function BeerCollection({ collection }) {
+export default function BeerCollection({ collection, pageCtrl }) {
+const nextPg = () => {
+  pageCtrl(1);
+}
+
+const previousPg = () => {
+  pageCtrl(-1)
+}
+
   return (
     <div>
       {collection.map((beer) => {
@@ -11,8 +19,8 @@ export default function BeerCollection({ collection }) {
         </div>
       })}
         <div className="pagination">
-          <div className="previous"/>
-            <div className="next"/>
+          <div className="previous" onClick={previousPg}/>
+            <div className="next" onClick={nextPg}/>
           </div>
     </div>
   );
