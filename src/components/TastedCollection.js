9d10dff8048b6240aab67984bcbe4e60e9d5dcff
zsofikassai <65860961+zsofikassai@users.noreply.github.com>
2021-08-31 10:57:02 +0200

    Create TastedCollection component

---
+++ src/components/TastedCollection.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { useState, useEffect } from "react";
+
+export const TastedCollection = () => {
+  const [tastedBeers, setTasted] = useState([]);
+  useEffect(() => {
+    localStorage.setItem("tastedBeers", JSON.stringify());
+  }, [tastedBeers]);
+
+  return <div></div>;
+};
