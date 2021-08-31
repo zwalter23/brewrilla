32ba9ca04b86b271443ffa850cd4cbed7e2c8867
catastroface <barna.urmossy@gmail.com>
2021-08-31 15:13:47 +0200

    Fix merge related problems

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import BeerCollection from "./components/BeerCollection";
 import { BeerDetails } from "./components/BeerDetails";
 import fetchData from "./components/fetchData";
@@ -35,11 +35,11 @@ function App() {
     const filter = document.getElementById("filters");
     let url = "";
 
-    if (filter.value == "brewed_before") {
+    if (filter.value === "brewed_before") {
       url = `https://api.punkapi.com/v2/beers?${filter.value}=12-${
         query.value - 1
       }`;
-    } else if (filter.value == "brewed_after") {
+    } else if (filter.value === "brewed_after") {
       url = `https://api.punkapi.com/v2/beers?${filter.value}=01-${query.value}`;
     } else {
       url = `https://api.punkapi.com/v2/beers?${filter.value}=${query.value}`;
@@ -48,28 +48,39 @@ function App() {
     fetchData(url).then((results) => {
       setBeerCollection(results.data);
       console.log(results.data);
+      console.log(beers);
     });
   };
 
   return (
     <Router>
-    <div className="layout">
-      <div className="navbar">
-        <p>Menu1</p>
-        <p>Menu2</p>
-        <p>Menu3</p>
-        <p>Menu4</p>
-        <p>Menu5</p>
+      <div className="layout">
+        <div className="navbar">
+          <Link to="/random">Random beer</Link>
+          <p>Menu2</p>
+          <p>Menu3</p>
+          <p>Menu4</p>
+          <p>Menu5</p>
+        </div>
+        <Route
+          path={"/"}
+          exact
+          render={() => (
+            <BeerCollection
+              collection={beers}
+              pageCtrl={changePage}
+              page={currentPage}
+              search={searchQuery}
+            />
+          )}
+        />
+        <Route
+          path={`/:id`}
+          render={(props) => <BeerDetails {...props} />}
+          exact
+        />
       </div>
-     <Route path="/" exact render={() => {
-      <BeerCollection collection={beers} pageCtrl={changePage} page={currentPage} search={searchQuery} />
-      </Route>
-     <Route
-        path={`/:id`}
-        render={(props) => <BeerDetails {...props} />}
-        exact />
-          </div>
-</Router>
+    </Router>
   );
 }
 
