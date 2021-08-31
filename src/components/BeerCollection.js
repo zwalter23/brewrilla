43f0a64065b498c6ad27fc312dd7276823f6d407
zwalter23 <zimmerwalter@gmail.com>
2021-08-31 13:24:22 +0200

    Search by filters

--- src/components/BeerCollection.js
+++ src/components/BeerCollection.js
@@ -1,28 +1,32 @@
 import React from "react";
+import Search from "./Search";
 
-export default function BeerCollection({ collection, pageCtrl, page }) {
-const nextPg = () => {
-  pageCtrl(1);
-}
+export default function BeerCollection({ collection, pageCtrl, page, search }) {
+  const nextPg = () => {
+    pageCtrl(1);
+  };
 
-const previousPg = () => {
-  pageCtrl(-1)
-}
+  const previousPg = () => {
+    pageCtrl(-1);
+  };
 
   return (
     <div>
+      <Search search={search} />
       {collection.map((beer) => {
-        return <div className="cards">
-          <div className="card">
-        <p>{beer.name}</p>
-        </div>
-        </div>
-      })}
-        <div className="pagination">
-          {page !== 1 && <div className="previous" onClick={previousPg}/>}
-          <p>Page {page}</p>
-            <div className="next" onClick={nextPg}/>
+        return (
+          <div className="cards">
+            <div className="card">
+              <p>{beer.name}</p>
+            </div>
           </div>
+        );
+      })}
+      <div className="pagination">
+        {page !== 1 && <div className="previous" onClick={previousPg} />}
+        <p>Page {page}</p>
+        <div className="next" onClick={nextPg} />
+      </div>
     </div>
   );
 }
