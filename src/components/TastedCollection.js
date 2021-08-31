0465c34f3365261fa8ce396bccd635b0955e931b
zsofikassai <65860961+zsofikassai@users.noreply.github.com>
2021-08-31 10:59:31 +0200

    Set tasted beers in localstorage

--- src/components/TastedCollection.js
+++ src/components/TastedCollection.js
@@ -4,8 +4,7 @@ import { useState, useEffect } from "react";
 export const TastedCollection = () => {
   const [tastedBeers, setTasted] = useState([]);
   useEffect(() => {
-    localStorage.setItem("tastedBeers", JSON.stringify());
+    localStorage.setItem("tastedBeers", JSON.stringify(tastedBeers));
   }, [tastedBeers]);
-
   return <div></div>;
 };
