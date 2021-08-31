dadf60738da966e6d0bd51d4eba2cf88cd72aa1a
catastroface <barna.urmossy@gmail.com>
2021-08-31 11:21:19 +0200

    Apply design to Navbar

--- src/App.js
+++ src/App.js
@@ -11,10 +11,12 @@ function App() {
   }, []);
 
   const getBeersByPage = (page) => {
-    const url = `https://api.punkapi.com/v2/beers?page=${page}&per_page=6`
-    fetchData(url).then((results)=>{setBeerCollection(results.data)})
+    const url = `https://api.punkapi.com/v2/beers?page=${page}&per_page=6`;
+    fetchData(url).then((results) => {
+      setBeerCollection(results.data);
+    });
     setCurrentPage(page);
-  }
+  };
 
   const changePage = (step) => {
     if (parseInt(step) < 0) {
@@ -27,8 +29,21 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <BeerCollection collection={beers} pageCtrl={changePage} page={currentPage} />
+    <div className="layout">
+      <div className="navbar">
+        <p>Menu1</p>
+        <p>Menu2</p>
+        <p>Menu3</p>
+        <p>Menu4</p>
+        <p>Menu5</p>
+      </div>
+      <div className="pagecontent">
+        <BeerCollection
+          collection={beers}
+          pageCtrl={changePage}
+          page={currentPage}
+        />
+      </div>
     </div>
   );
 }
