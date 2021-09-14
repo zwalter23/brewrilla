750bd90a1c1e14ff54d0e94773415ea318d91e41
zsofikassai <65860961+zsofikassai@users.noreply.github.com>
2021-09-14 11:57:19 +0200

    Create BeerDbContext

--- .Net core/Brewrilla/Models/BeerDbContext.cs
+++ .Net core/Brewrilla/Models/BeerDbContext.cs
@@ -1,11 +1,18 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Brewrilla.Models
 {
-    public class BeerDbContext
+    public class BeerDbContext:DbContext
     {
+        public BeerDbContext(DbContextOptions<BeerDbContext> options):base(options)
+        {
+                
+        }
+
+        public DbSet<DBeer> DBeers { get; set; }
     }
 }
