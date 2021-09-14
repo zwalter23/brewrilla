924401ba71a1aa2a8c0e117c7ce140afdd979beb
zsofikassai <65860961+zsofikassai@users.noreply.github.com>
2021-09-14 11:53:34 +0200

    Create database schema

---
+++ .Net core/Brewrilla/Models/DBeer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Brewrilla.Models
+{
+    public class DBeer
+    {
+        [Key]
+        public int id { get; set; }
+        public string beerName { get; set; }
+        public string tagline { get; set; }
+
+        public string description { get; set; }
+        public string imageUrl { get; set; }
+        public string firstBrewed { get; set; }
+
+        public float abv { get; set; }
+
+        public int ibu { get; set; }
+
+        public string[] ingredients { get; set; }
+        public string[] foodPairings { get; set; }
+
+        public int targetOG { get; set; }
+        public int targetFG { get; set; }
+
+        public int ebc { get; set; }
+        public int srm { get; set; }
+        public float ph { get; set; }
+        public string brewersTip { get; set; }
+
+    }
+}
