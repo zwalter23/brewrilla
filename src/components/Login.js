2afdcd8fe26a8a9f79187f22c373efa7edaec99b
zsofikassai <65860961+zsofikassai@users.noreply.github.com>
2021-09-15 11:45:41 +0200

    Create login form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,22 @@
 import React from "react";
 
 export const Login = () => {
-  return <div>Login page</div>;
+  return (
+    <div className="login-wrapper">
+      <h1>Login page</h1>
+      <form>
+        <label>
+          <p>Username</p>
+          <input type="text" />
+        </label>
+        <label>
+          <p>Password</p>
+          <input type="password" />
+        </label>
+        <div>
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+    </div>
+  );
 };
